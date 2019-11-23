e9692b42ec4b4f17d8a2141550b41ca1f84d5e62
raman47 <ramanjs1313@gmail.com>
2019-11-23 17:42:58 +0530

    create dummy data for posts and users table

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //reset the users table
+        DB::table('users')->truncate();
+
+        DB::table('users')->insert(
+        [
+            'name' => "john doe",
+            'email' => "johndoe@test.com",
+            'password' => bcrypt('secret')
+        ],
+        [
+            'name' => "Jane doe",
+            'email' => "janedoe@test.com",
+            'password' => bcrypt('secret')
+        ],
+        [
+            'name' => "ed doe",
+            'email' => "eddoe@test.com",
+            'password' => bcrypt('secret')
+        ],
+    
+        );
+    }
+}
